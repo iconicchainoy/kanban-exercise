c25b4e4bb6ee7168ad5869e759629128f0a7d534
Eero <eero.helske@gmail.com>
2022-05-25 13:53:47 +0300

    setup react with yarn and webpack

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: "./src/index.js",
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve("dist"),
+    publicPath: "/",
+  },
+  module: {
+    rules:[
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    inject: true,
+    template: './public/index.html',
+  })],
+}
